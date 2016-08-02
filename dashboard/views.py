7ca50fe096aed140d659bbfb5f921b7c307cff73
Your Name <you@example.com>
2016-08-02 14:40:07 -0700

    dashboard fooling around

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 
-from django.views.generic import View
+from django.views.generic import TemplateView
 from django.http.response import HttpResponsePermanentRedirect
 
 # Create your views here.
+from vinculum.models import Vinculum
 
-class DashboardView(View):
+
+class DashboardView(TemplateView):
     template_name = 'dashboard.html'
 
-    def get(self, request):
-        if request.user.is_authenticated() is False:
-            return HttpResponsePermanentRedirect("/")
-        else:
-            return render(request=request, template_name="dashboard.html")
+    def get_context_data(self, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        context['vinculums'] = Vinculum.objects.filter(owner=self.request.user)
+        return context
